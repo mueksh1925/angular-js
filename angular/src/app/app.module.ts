6b371482642a2386d297ab447c6a403ddb671e36
mueksh1925 <mukesh.krs2008@gmail.com>
2017-12-28 10:13:36 +0530

    Update app.module.ts

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import {HeaderComponent} from './header/header.component';
 import { HomeComponent } from './home/home.component'
-import {dataService} from './service/dataService.service'
+import {dataService} from './service/dataService.service';
+import {routing,routerProvider} from './router';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,HttpModule ],
+  imports:      [ BrowserModule, FormsModule,HttpModule,routing ],
   declarations: [ AppComponent,HelloComponent,HeaderComponent, HomeComponent ],
-  providers:[dataService],
+  providers:[dataService,routerProvider],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
