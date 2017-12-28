6d669eeada079ee1dedef65c7aa258199527d87c
mueksh1925 <mukesh.krs2008@gmail.com>
2017-12-28 10:20:11 +0530

    Update app.module.ts

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,7 +5,12 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import {HeaderComponent} from './header/header.component';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
 import {dataService} from './service/dataService.service';
 import {routing,routerProvider} from './router';
 @NgModule({
