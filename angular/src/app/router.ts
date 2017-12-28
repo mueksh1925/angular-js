24c42ea027263b29cdc22f8b70bb0444823b499d
mueksh1925 <mukesh.krs2008@gmail.com>
2017-12-28 10:11:13 +0530

    router file

---
+++ angular/src/app/router.ts
@@ -0,0 +1,17 @@
+import{Routes,RouterModule} from '@angular/router';
+import{HomeComponent} from './home/home.component';
+import{AboutComponent} from './about/about.component';
+import{ContactComponent} from './contact/contact.component';
+import{LoginComponent} from './login/login.component';
+export const routes:Routes=
+                      [
+                        {path:'',redirectTo:'home',pathMatch:'full'},
+                        {path:'home',component:HomeComponent},
+                        {path:'about',component:AboutComponent},
+                        {path:'contact',component:ContactComponent},
+                        {path:'login',component:LoginComponent}
+                      ]
+export const routing = RouterModule.forRoot(routes);
+export class routerProvider{
+
+}
